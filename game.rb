0139035f86058165752af57ec7c5142efce6738c
Roberto Ruiz <artificemm@gmail.com>
2021-03-08 22:06:02 -0600

    setting up rules for clearing up what should I do

--- game.rb
+++ game.rb
@@ -1,3 +1,12 @@
 # Bowling challenge
 # Applicant: Roberto Ruiz, artificemm@gmail.com
 
+# 1) Open file from arguments
+
+# 2) Parse file into numeric values
+
+# 3) Set Frames with 2 rolls each. From frame 1 up to 9
+
+# 4) Frame 10 is a special case with up to 3 rolls, depends on either strike or spare rolls
+
+# 5) Print scoreboard
